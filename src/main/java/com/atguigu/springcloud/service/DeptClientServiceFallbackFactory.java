d0de4b9ece5a7760b8fc85c9a0b4b14557892321
shangweizheng <siwad@126.com>
2019-06-18 17:54:22 +0800

    first

---
+++ src/main/java/com/atguigu/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,37 @@
+package com.atguigu.springcloud.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.atguigu.springcloud.entities.Dept;
+
+import feign.hystrix.FallbackFactory;
+
+@Component // 不要忘记添加，不要忘记添加
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService>
+{
+	@Override
+	public DeptClientService create(Throwable throwable)
+	{
+		return new DeptClientService() {
+			@Override
+			public Dept get(long id)
+			{
+				return null;
+			}
+
+			@Override
+			public List<Dept> list()
+			{
+				return null;
+			}
+
+			@Override
+			public boolean add(Dept dept)
+			{
+				return false;
+			}
+		};
+	}
+}
